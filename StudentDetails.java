61d0aac74b839ac6071ce400d5f1923f46728cd5
Ishrath Tabassum <148183636+Azalea2406@users.noreply.github.com>
2024-11-21 18:52:35 +0530

    StudentDetails.java

--- StudentDetails.java
+++ StudentDetails.java
@@ -5,7 +5,7 @@ class Student {
     String rollNumber;
     String name;
     String department;
-
+//construuctor
     public Student(String rollNumber, String name, String department) {
         this.rollNumber = rollNumber;
         this.name = name;
@@ -20,9 +20,9 @@ class Student {
 
 public class StudentDetails {
     public static void main(String[] args) {
-
+// HashMap to store student data, rollnumber as keys and StudentName as values
         Map<String, Student> studentMap = new HashMap<>();
-
+//CSV files corresponding to Department names
         String[] Files = {"CSE.csv", "CME.csv"};
         String[] departments = {"CSE", "CME"};
 
@@ -32,7 +32,7 @@ public class StudentDetails {
 
             try (BufferedReader br = new BufferedReader(new FileReader(File))) {
                 String line;
-
+//Read each line from the CSV file
                 while ((line = br.readLine()) != null) {
                     String[] data = line.split(",");
                     if (data.length == 2) { 
@@ -45,11 +45,12 @@ public class StudentDetails {
                 System.out.println("Error reading the file");
             }
         }
-
+        // 'scanner' object to take input from the user
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Roll Number to search: ");
         String search = scanner.nextLine().trim();
 
+        // if student found print their details else pritn Student not found
         if (studentMap.containsKey(search)) {
             System.out.println("\nStudent Details: ");
             System.out.println(studentMap.get(search));  
